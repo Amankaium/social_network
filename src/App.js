551f1c6b2086aad3d468dcdf93e24129245fcd4f
Kaium <kaium@list.ru>
2019-04-15 09:47:35 +0600

    added routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 import Header from './compotents/Header/Header';
 import Navbar from './compotents/Navbar/Navbar';
 import Profile from './compotents/Profile/Profile';
+import Messages from './compotents/Messages/Messages';
 
 const App = () => {
     return (
-        <div className="app-wrapper"> 
-            <Header/>
-            <Navbar/>
-            <Profile/>            
-        </div>
+        <BrowserRouter>
+            <div className="app-wrapper"> 
+                <Header/>
+                <Navbar/>
+                <div className="app-wrapper-content">
+                    <Route path='/profile' component={Profile}/>
+                    <Route path='/messages' component={Messages}/>
+                    {/* <Route path='/news' component={News}/>
+                    <Route path='/music' component={Music}/>
+                    <Route path='/settings' component={Settings}/> */}
+                </div>                                    
+            </div>
+        </BrowserRouter>
     );
 }
 
